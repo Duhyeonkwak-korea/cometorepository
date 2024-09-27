293f338d18bd33ad9344b45d63f0ace239ddb651
duhyeonkwak-korea <duhyeonkwak@gmail.com>
2024-09-27 17:05:36 +0900

    2주차 django 개발완료

---
+++ product/views.py
@@ -0,0 +1,15 @@
+from importlib.resources import contents
+from lib2to3.fixes.fix_input import context
+
+from django.shortcuts import render
+
+# Create your views here.
+
+#from django.http import HttpResponse
+from django.shortcuts import render
+from .models import MainContent
+
+def index(request):
+    content_list = MainContent.objects.order_by('-pub_date')
+    context = {'content_list':content_list}
+    return render(request, 'product/content_list.html',context)
