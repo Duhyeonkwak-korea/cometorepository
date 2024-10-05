581d65e31a3b99232d260fa2c5dec8f32c3d6792
duhyeonkwak-korea <duhyeonkwak@gmail.com>
2024-10-05 18:30:56 +0900

    Cometo 3주차 미션

--- my_project/settings.py
+++ my_project/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'product',
-    'pages'
+    'pages',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -128,3 +129,6 @@ STATICFILES_DIRS =[
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
